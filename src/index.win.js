ddc8a2e9d214ce6e0591adca505d6f208bff81ef
huang ping <766137195@qq.com>
2019-06-19 16:26:23 +0800

    'electron'

---
+++ src/index.win.js
@@ -0,0 +1,22 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+
+function createWindow () {
+  //创建浏览器窗口
+  win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  //让浏览器加在项目
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+}
+
+//执行
+app.on('ready', createWindow);
